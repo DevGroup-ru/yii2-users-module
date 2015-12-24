5eaeb5a3f30f09090a4461c34c742ff75473ed65
bethrezen <b37hr3z3n@gmail.com>
2015-12-24 12:09:14 +0300

    Initial

---
+++ src/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DevGroup\Users;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
